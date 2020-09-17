1ab1eff873a39244613e3e85838b7664de18fcb0
Nils Magnus <magnus@linuxtag.org>
2020-09-17 09:57:55 +0200

    add game logic

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, render_template
+import yaml
+
+app = Flask(__name__)
+
+with open("config.yaml") as file:
+    config = yaml.load(file, Loader=yaml.FullLoader)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        secret = ""
+    else:
+        secret = request.form["secret"]
+
+    fragen = config["fragen"]
+
+    frage = next((i for i, f in enumerate(fragen) if f["s"] == secret), 0)
+    
+    return render_template("index.html",
+                           thislevel=frage,
+                           nextlevel=frage + 1,
+                           aufgabe=fragen[frage]["q"],
+                           debug=("secret = '" + secret + "' fragen = " + str(fragen) + "frage #" + str(frage)))
+
+@app.route("/item")
+def item():
+    return render_template("item.html")
+    
+if __name__ == '__main__':
+    app.run(port=8000, debug=True)
