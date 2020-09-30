c89df7d2866a4ea08256a53130328e65a8bfc4a0
Nils Magnus <magnus@linuxtag.org>
2020-09-30 12:33:57 +0000

    add extra code for success page

--- main.py
+++ main.py
@@ -10,22 +10,42 @@ with open("config.yaml") as file:
 def index():
     if request.method == "GET":
         secret = ""
+        levelByInput  = 0
     else:
         secret = request.form["secret"]
+        try:
+            levelByInput = int(request.form["level"])
+        except ValueError:
+            levelByInput = 0
 
     fragen = config["fragen"]
 
-    frage = next((i for i, f in enumerate(fragen) if f["s"] == secret), 0)
+    levelByAnswer = next((i for i, f in enumerate(fragen) if f["s"] == secret), 0)
 
+    if levelByAnswer == levelByInput:
+        thisLevel = levelByAnswer + 1
+        wrong = None
+    else:
+        thisLevel = levelByAnswer
+        wrong = secret
+    
     debugmsg = ""
     if config["debug"]:
-        debugmsg = "secret = '" + secret + "' fragen = " + str(fragen) + "frage #" + str(frage)
+        debugmsg = "secret = '" + secret + "' fragen = " + str(fragen) + "frage #" + str(levelByAnswer)
 
     return render_template("index.html",
-                           thislevel=frage,
-                           nextlevel=frage + 1,
-                           aufgabe=fragen[frage]["q"],
+                           thislevel=thisLevel,
+                           nextlevel=thisLevel + 1,
+                           aufgabe=fragen[thisLevel]["q"],
+                           wronganswer=wrong,
                            debug=debugmsg)
+
+
+@app.route("/demosuccess")
+def demosuccess():
+    return render_template("success.html",
+                           secretkey="-".join(i["s"] for i in config["fragen"]))
+
 
 @app.route("/item")
 def item():
