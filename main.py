40ee36eb76b43251e87a80ab03dc938c148f819d
Nils Magnus <magnus@linuxtag.org>
2020-10-05 12:51:30 +0000

    fix the game logic

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from flask import Flask, request, render_template
 import yaml
 
+
+def qIndexFor(sec):
+    fragen = config["fragen"]
+
+    level = next((i for i, f in enumerate(fragen) if f["s"] == sec), -1)
+
+    return level 
+    
+
 app = Flask(__name__)
 
 with open("config.yaml") as file:
@@ -8,38 +17,57 @@ with open("config.yaml") as file:
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    # find out which level we've been so far as documented by the hidden value
     if request.method == "GET":
         secret = ""
-        levelByInput  = 0
+        levelByHidden  = -1
     else:
-        secret = request.form["secret"]
+        secret = request.form["secret"].upper()
         try:
-            levelByInput = int(request.form["level"])
+            levelByHidden = int(request.form["level"])
         except ValueError:
-            levelByInput = 0
+            levelByHidden = -1
 
-    fragen = config["fragen"]
+    msg = ""
+    if secret:
+        msg = "The answer '%s' is unfortunately wrong. Try again:" % secret
+        
+    # find out which level we solved based on the answer:
+    # -1 no answer at all
+    # 0..n a right answer, but does it qualify?
+    levelByAnswer = qIndexFor(secret)
+    if levelByAnswer < 0:
+        if levelByHidden < 0:
+            newLevel = 0
+        else:
+            newLevel = levelByHidden
+    else: # wir haben eine richtige antwort abgegeben, aber passt der letzte level auch dazu?
+        if levelByAnswer == levelByHidden:
+            newLevel = levelByAnswer + 1
+            msg = "The answer '%s' is correct!" % secret
+        else: # eine korrekte antwort, aber letzter level stimmt nicht? Gemogelt?
+            newLevel = levelByHidden
 
-    levelByAnswer = next((i for i, f in enumerate(fragen) if f["s"] == secret), 0)
-
-    if levelByAnswer == levelByInput:
-        thisLevel = levelByAnswer + 1
-        wrong = None
-    else:
-        thisLevel = levelByAnswer
-        wrong = secret
-    
-    debugmsg = ""
+    maxLevel = len(config["fragen"])
+            
+    debugmsg = list()
     if config["debug"]:
-        debugmsg = "secret = '" + secret + "' fragen = " + str(fragen) + "frage #" + str(levelByAnswer)
-
-    return render_template("index.html",
-                           thislevel=thisLevel,
-                           nextlevel=thisLevel + 1,
-                           aufgabe=fragen[thisLevel]["q"],
-                           wronganswer=wrong,
-                           debug=debugmsg)
-
+        debugmsg.append("Secret:        " + secret)
+        debugmsg.append("levelByHidden: " + str(levelByHidden))
+        debugmsg.append("levelByAnswer: " + str(levelByAnswer))
+        debugmsg.append("newLevel:      " + str(newLevel))
+        debugmsg.append("maxLevel:      " + str(maxLevel))
+        
+    if newLevel == maxLevel:
+        return render_template("success.html",
+                               secretkey="-".join(i["s"] for i in config["fragen"]),
+                               solution=config["solution"])
+    else:
+        return render_template("index.html",
+                               level=newLevel,
+                               aufgabe=config["fragen"][newLevel]["q"],
+                               answer=msg,
+                               debug=debugmsg)
 
 @app.route("/demosuccess")
 def demosuccess():
